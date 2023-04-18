3fa9f5d70180deaa85edc2a461da0034661339de
jessegrabowski <jessegrabowski@gmail.com>
2023-04-19 00:25:24 +0200

    Add version  `__init__`

--- gEconpy/__init__.py
+++ gEconpy/__init__.py
@@ -11,6 +11,7 @@ from gEconpy import (
 from gEconpy.classes import gEconModel
 from gEconpy.shared import compile_to_statsmodels, make_mod_file
 
+__version__ = "1.1.0"
 __all__ = [
     "gEconModel",
     "classes",
