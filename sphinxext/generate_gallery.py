2256b2d43b10c5ef438b19542517f0a272fcc5cb
jessegrabowski <jessegrabowski@gmail.com>
2025-03-13 13:00:02 +0800

    Add case_study group to gallery

--- sphinxext/generate_gallery.py
+++ sphinxext/generate_gallery.py
@@ -54,7 +54,11 @@ ITEM_TEMPLATE = """
       :shadow: none
 """
 
-folder_title_map = {"introductory": "Introductory", "estimation": "Estimation"}
+folder_title_map = {
+    "introductory": "Introductory",
+    "estimation": "Estimation",
+    "case_study": "Case Studies",
+}
 
 
 def create_thumbnail(infile, width=275, height=275, cx=0.5, cy=0.5, border=4):
