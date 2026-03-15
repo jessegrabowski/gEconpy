8c9a1623933867c597d5979ba178f1637b4a3f2c
jessegrabowski <jessegrabowski@gmail.com>
2026-03-14 20:39:22 -0500

    Add Chord solver with cached Jacobian direction and ChordDirection strategy

--- tests/solvers/sparse_root/test_direction.py
+++ tests/solvers/sparse_root/test_direction.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.sparse as sp
 
-from gEconpy.solvers.sparse_root.direction import NewtonDirection
+from gEconpy.solvers.sparse_root.direction import ChordDirection, NewtonDirection
 
 
 class TestNewtonDirectionFallback:
@@ -21,3 +21,32 @@ class TestNewtonDirectionFallback:
         jac = sp.csc_matrix([[-1.0, 0.0], [0.0, -1.0]])
         proposal = direction.compute(np.zeros(2), np.array([1.0, 1.0]), jac)
         assert proposal.slope < 0
+
+
+class TestChordDirectionSpecific:
+    def test_cache_persists(self):
+        """Cached Jacobian should be reused for recompute_every steps."""
+        cd = ChordDirection(recompute_every=3)
+        cd.reset()
+
+        jac1 = sp.eye(2, format="csc")
+        jac2 = 2.0 * sp.eye(2, format="csc")
+        res = np.array([1.0, 1.0])
+        x = np.zeros(2)
+
+        # Call 0: _call_count=0, 0%3==0 -> cache jac1, increment to 1
+        cd.compute(x, res, jac1)
+        assert cd._cached_jac is not None
+        cached_data = cd._cached_jac.data.copy()
+
+        # Call 1: _call_count=1, 1%3!=0 -> reuse jac1, increment to 2
+        cd.compute(x, res, jac2)
+        np.testing.assert_array_equal(cd._cached_jac.data, cached_data)
+
+        # Call 2: _call_count=2, 2%3!=0 -> still reuse jac1, increment to 3
+        cd.compute(x, res, jac2)
+        np.testing.assert_array_equal(cd._cached_jac.data, cached_data)
+
+        # Call 3: _call_count=3, 3%3==0 -> refresh to jac2, increment to 4
+        cd.compute(x, res, jac2)
+        np.testing.assert_array_equal(cd._cached_jac.data, jac2.data)
