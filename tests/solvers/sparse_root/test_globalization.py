481fddf224453f6289651b684419754c77adfcad
jessegrabowski <jessegrabowski@gmail.com>
2026-03-14 20:41:34 -0500

    Add nonmonotone backtracking line search and NewtonNonmonotone solver

--- tests/solvers/sparse_root/test_globalization.py
+++ tests/solvers/sparse_root/test_globalization.py
@@ -3,7 +3,7 @@ import pytest
 import scipy.sparse as sp
 
 from gEconpy.solvers.sparse_root.direction import DirectionProposal
-from gEconpy.solvers.sparse_root.globalization import ArmijoBacktracking
+from gEconpy.solvers.sparse_root.globalization import ArmijoBacktracking, NonmonotoneBacktracking
 
 
 class TestArmijoBacktracking:
@@ -26,3 +26,29 @@ class TestArmijoBacktracking:
         proposal = DirectionProposal(np.array([1.0]), slope=-1.0, kind="newton")
         with pytest.raises(RuntimeError):
             ls.search(fun, np.array([0.0]), phi_current=0.5, proposal=proposal, args=())
+
+
+class TestNonmonotoneBacktracking:
+    def test_memory_one_equals_armijo(self):
+        """With memory=1, nonmonotone backtracking should behave like standard Armijo."""
+
+        def fun(x):
+            return x.copy(), sp.eye(len(x), format="csc")
+
+        x = np.array([2.0])
+        proposal = DirectionProposal(direction=np.array([-1.0]), slope=-2.0, kind="newton")
+
+        armijo = ArmijoBacktracking(c1=1e-4, beta=0.5, max_iter=50)
+        nm = NonmonotoneBacktracking(c1=1e-4, beta=0.5, max_iter=50, memory=1)
+
+        result_a = armijo.search(fun, x, phi_current=2.0, proposal=proposal, args=())
+        result_nm = nm.search(fun, x, phi_current=2.0, proposal=proposal, args=())
+
+        np.testing.assert_allclose(result_a.alpha, result_nm.alpha)
+
+    def test_memory_growth(self):
+        """History should not exceed memory size."""
+        nm = NonmonotoneBacktracking(memory=3)
+        for i in range(10):
+            nm._phi_history.append(float(i))
+        assert len(nm._phi_history) <= 3
